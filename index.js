d8e87c3b193211c444846fc555841b28e21d727c
Bass <sambass1992@gmail.com>
2017-12-18 12:02:51 -0600

    changed greeting

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ bye: 'buddy' });
 });
 
 // DYNAMIC PORT BINDING
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+/*
+Creating app... done, ⬢ limitless-hamlet-26743
+https://limitless-hamlet-26743.herokuapp.com/ | https://git.heroku.com/limitless-hamlet-26743.git
+*/
