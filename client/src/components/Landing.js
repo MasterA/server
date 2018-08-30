eb62b022928ba8431ad58aa4fd295cea2f71f1a8
Bass <sambass1992@gmail.com>
2018-08-30 10:52:29 -0500

    back to the grind.

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,6 +4,9 @@ const Landing = () => {
   const logo2 = require('./img/sketchin.jpg');
   return (
       <div style={{ textAlign: 'center', backgroundColor: '#000000' }}>
+      	<center>
+      	Hi! THERE!
+      	</center>
         <center>
           <img alt='logo2' src={String(logo2)} style={{ width: '100%', height: 'auto' }}/>
         </center>
