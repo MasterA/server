a532d6563340db01a1e7e5358351b270cfd18f34
Bass <sambass1992@gmail.com>
2018-08-31 10:26:11 -0500

    adding card in correct spot

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,9 +4,9 @@ import Card from 'Card';
 const Landing = () => {
   const logo2 = require('./img/sketchin.jpg');
   return (
-      <Card />
       <div style={{ textAlign: 'center', backgroundColor: '#000000' }}>
         <center>
+          <Card />
           //<img alt='logo2' src={String(logo2)} style={{ width: '100%', height: 'auto' }}/>
         </center>
       </div>
