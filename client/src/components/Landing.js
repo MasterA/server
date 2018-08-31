4561d6cf8c41b6631111c1db7d14dcfbf62fd6d7
Bass <sambass1992@gmail.com>
2018-08-31 10:30:23 -0500

    fixing card component

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Card from 'Card';
+import card from 'Card';
 
 const Landing = () => {
   const logo2 = require('./img/sketchin.jpg');
   return (
       <div style={{ textAlign: 'center', backgroundColor: '#000000' }}>
         <center>
-          <Card />
+          <card />
           //<img alt='logo2' src={String(logo2)} style={{ width: '100%', height: 'auto' }}/>
         </center>
       </div>
