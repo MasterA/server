7dcd4c46235f9c2f6abbbf189412c4c451135e8c
Bass <sambass1992@gmail.com>
2018-08-31 10:22:44 -0500

    adding card to landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Card from 'Card';
 
 const Landing = () => {
   const logo2 = require('./img/sketchin.jpg');
   return (
+      <Card />
       <div style={{ textAlign: 'center', backgroundColor: '#000000' }}>
-      	<center>
-      	Hi! THERE!
-      	</center>
         <center>
           //<img alt='logo2' src={String(logo2)} style={{ width: '100%', height: 'auto' }}/>
         </center>
